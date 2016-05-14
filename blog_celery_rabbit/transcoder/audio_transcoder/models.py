c5fe4e8d771e72578082f16bcf8dc1db89a35d83
Krzysztof Żuraw <krzysztof.zuraw@gmail.com>
2016-05-14 09:18:19 +0200

    Rename function for upload location

--- blog_celery_rabbit/transcoder/audio_transcoder/models.py
+++ blog_celery_rabbit/transcoder/audio_transcoder/models.py
@@ -3,16 +3,16 @@ import uuid
 from django.db import models
 
 
-def md5_directory_path(instance, filename):
+def unique_file_path(instance, filename):
     new_file_name = uuid.uuid4()
     return str(new_file_name)
 
 class AudioFile(models.Model):
     name = models.CharField(max_length=100, blank=True)
-    mp3_file = models.FileField(upload_to=md5_directory_path)
-    ogg_file = models.FileField(blank=True, upload_to=md5_directory_path)
-    wav_file = models.FileField(blank=True, upload_to=md5_directory_path)
-    ac3_file = models.FileField(blank=True, upload_to=md5_directory_path)
+    mp3_file = models.FileField(upload_to=unique_file_path)
+    ogg_file = models.FileField(blank=True, upload_to=unique_file_path)
+    wav_file = models.FileField(blank=True, upload_to=unique_file_path)
+    ac3_file = models.FileField(blank=True, upload_to=unique_file_path)
     was_processed = models.BooleanField(default=False)
 
     def __str__(self):

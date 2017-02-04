7368340b7c65106b1cd2e6fe35d4a38c4ac7bcc9
Krzysztof Żuraw <krzysztof.zuraw@gmail.com>
2017-02-04 10:59:41 +0100

    Add proper json messages + makefile commands

--- lru_cache/application.py
+++ lru_cache/application.py
@@ -1,11 +1,36 @@
-from flask import Flask
+from flask import Flask, jsonify
+
+from functools import lru_cache
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def main():
-    return 'OK'
+    store_to_cache()
+    return jsonify({'message': 'Stored'})
+
+
+@lru_cache(maxsize=2)
+def store_to_cache():
+    return {'this_goes_to_cache': 'and_this_too'}
+
+
+@app.route('/get_cache_info')
+def get_cache_info():
+    cache_info = store_to_cache.cache_info()
+    return jsonify({
+        'Hits': cache_info.hits,
+        'Misses': cache_info.misses,
+        'Maxsize': cache_info.maxsize,
+        'Currsize': cache_info.currsize
+    })
+
+
+@app.route('/clear_cache')
+def clear_cache():
+    store_to_cache.cache_clear()
+    return jsonify({'message': 'Cache cleared'})
 
 
 if __name__ == '__main__':
